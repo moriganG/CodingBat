794027c5f3f8943f765b5f7203c20d67b90c663a
moriganG <84204630+moriganG@users.noreply.github.com>
2021-06-05 23:02:28 +0200

    Create bigHeights.java

---
+++ AP-1/bigHeights.java
@@ -0,0 +1,13 @@
+// (A variation on the sumHeights problem.) We have an array of heights, representing the altitude along a walking trail. 
+// Given start/end indexes into the array, return the number of "big" steps for a walk starting at the start index and ending at the end index. 
+// We'll say that step is big if it is 5 or more up or down. The start end end index will both be valid indexes into the array with start <= end.
+
+public int bigHeights(int[] heights, int start, int end) {
+  int counter = 0;
+  for (int i = start; end > i; i++){
+    if (Math.abs(heights[i + 1] - heights[i]) >= 5){
+      counter++;
+    }
+  }
+  return counter;
+}
